7771e4bf71da1afe38be7fb0d1d8d464bce29e10
Re:ZERO <88499887+takazo3@users.noreply.github.com>
2022-03-17 22:48:53 +0900

    Add files via upload
    using and handling error

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import pandas
 
-
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_phonetic = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
-user_word = [nato_phonetic[letter] for letter in word]
 
-print(user_word)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+
+    try:
+        user_word = [nato_phonetic[letter] for letter in word]
+
+    except KeyError:
+        print("Sorry, only letter in alphabet please. ")
+        generate_phonetic()
+    else:
+        print(user_word)
+
+
+generate_phonetic()
